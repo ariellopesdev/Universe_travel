529db5272edd8db38a4305a15b92b531001bb6a3
lubyelcuritiba <lubyel.lopes@gmail.com>
2022-11-18 19:16:01 -0300

    Finalized

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include('config.php'); ?>
+<html>
+
+<head>
+    <title>Universe Travel</title>
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>fonts/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+    <link href="<?php echo INCLUDE_PATH; ?>style.css" rel="stylesheet" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras-chave, do, meu, site">
+    <meta name="description" content="Descrição do meu website">
+    <meta name="author" content="Ariel Lopes">
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <div class="scene">
+        <div class="wrap">
+            <div class="wall wall-right"></div>
+            <div class="wall wall-left"></div>
+            <div class="wall wall-top"></div>
+            <div class="wall wall-bottom"></div>
+            <div class="wall wall-back"></div>
+        </div>
+        <div class="wrap">
+            <div class="wall wall-right"></div>
+            <div class="wall wall-left"></div>
+            <div class="wall wall-top"></div>
+            <div class="wall wall-bottom"></div>
+            <div class="wall wall-back"></div>
+        </div>
+    </div>
+</body>
+
+</html>
